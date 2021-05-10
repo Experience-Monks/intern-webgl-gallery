13b46e4a009268588259693d0b4116b15ffba4d8
Mariana Queiroz <mari.p.dequeiroz@gmail.com>
2021-05-10 15:33:55 -0400

    Refactor landing page
    Refactor landing page to use simple CSS.
    
     Add placeholder cat photos for thumbnails.
    
     Update data/gallery.js for more variable information

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,17 +1,20 @@
 export default {
   Artwork_1: {
     path: '/gallery/artwork1/',
-    title: 'Artwork 1',
-    author: 'name'
+    title: 'Amna Art',
+    author: 'name',
+    thumbnail: 'https://placekitten.com/220/220'
   },
   Artwork_2: {
     path: '/gallery/artwork2/',
-    title: 'Artwork 2',
-    author: 'name'
+    title: 'Mariana art',
+    author: 'Mariana @pannunzio',
+    thumbnail: 'https://placekitten.com/220/220'
   },
   Artwork_3: {
     path: '/gallery/artwork3/',
-    title: 'Artwork 3',
-    author: 'name'
+    title: 'Mia Art',
+    author: 'name',
+    thumbnail: 'https://placekitten.com/220/220'
   }
 };
