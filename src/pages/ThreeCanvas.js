c72712d0bb7cdaf1f2dbce8e80914b78620afd5c
Mariana Queiroz <mari.p.dequeiroz@gmail.com>
2021-05-19 01:16:39 -0400

    Subject: First iteration of gallery landing Type: Update
    Add
    Subject: First iteration of gallery landing
    Type: Update
    
    - Add gallery interface
    - fix gallery CSS
    - fix main gallery wrapper
    - remove useless components
    - update index.js to make more sense
    - fun
    
    fix

--- src/pages/ThreeCanvas.js
+++ src/pages/ThreeCanvas.js
@@ -1,37 +1,187 @@
+import React, { useRef, useEffect } from 'react';
+
 import {
-  LinearFilter,
-  LoadingManager,
-  OrthographicCamera,
-  PlaneGeometry,
+  Color,
+  Fog,
+  AmbientLight,
   Scene,
-  TextureLoader,
+  Clock,
   Vector2,
   PerspectiveCamera,
   BoxGeometry,
-  MeshBasicMaterial,
   Mesh,
-  WebGLRenderer
+  Group,
+  WebGLRenderer,
+  Vector3,
+  DirectionalLight,
+  TextureLoader,
+  Raycaster,
+  MeshLambertMaterial,
+  PlaneGeometry,
+  WireframeGeometry,
+  LineBasicMaterial,
+  LineSegments,
+  ReinhardToneMapping,
+  FontLoader
 } from 'three/build/three.module';
 
-export default function Art(sceneRef) {
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  sceneRef.appendChild(renderer.domElement);
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
 
-  const geometry = new BoxGeometry();
-  const material = new MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
+function Art() {
+  const inputEl = useRef(null);
 
-  camera.position.z = 5;
+  useEffect(() => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, inputEl.current.offsetWidth / inputEl.current.offsetHeight, 0.1, 1000);
+    const renderer = new WebGLRenderer({ antialias: true });
 
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-  animate();
+    renderer.setPixelRatio(inputEl.current.offsetWidth / inputEl.current.offsetHeight);
+    renderer.setSize(inputEl.current.offsetWidth, inputEl.current.offsetHeight);
+    inputEl.current.append(renderer.domElement);
+    renderer.toneMapping = ReinhardToneMapping;
+
+    inputEl.current.addEventListener('mousemove', onPointerMove);
+    inputEl.current.addEventListener('resize', onWindowResize);
+    inputEl.current.addEventListener('click', onMouseClick);
+
+    const controls = new OrbitControls(camera, inputEl.current);
+    const raycaster = new Raycaster();
+    const directionalLight = new DirectionalLight(0xff00ff, 0.5);
+    const group = new Group();
+    const mouse = new Vector2();
+    let INTERSECTED;
+    let wireframe;
+    let matLineBasic;
+    let composer;
+    const clock = new Clock();
+    const params = {
+      exposure: 1,
+      bloomStrength: 1.5,
+      bloomThreshold: 0,
+      bloomRadius: 0
+    };
+
+    function onWindowResize() {
+      camera.aspect = inputEl.current.offsetWidth / inputEl.current.offsetHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(inputEl.current.offsetWidth, inputEl.current.offsetHeight);
+
+      composer.setSize(inputEl.current.offsetWidth, inputEl.current.offsetHeight);
+    }
+
+    function onMouseClick(event) {
+      console.log('mouse click');
+
+      if (INTERSECTED) {
+        window.location = INTERSECTED.userData.url;
+      }
+    }
+
+    function onPointerMove(event) {
+      mouse.x = (event.clientX / inputEl.current.offsetWidth) * 2 - 1;
+      mouse.y = -(event.clientY / inputEl.current.offsetHeight) * 2 + 1;
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+      render();
+
+      composer.render();
+    }
+
+    function render() {
+      //     camera.lookAt(scene.position);
+
+      camera.position.x += ( mouse.x - camera.position.x ) * 0.5;
+      camera.position.y += ( -mouse.y - camera.position.y ) * 1.5;
+      camera.lookAt(scene.position);
+
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects(group.children);
+
+      if (intersects.length > 0) {
+        if (INTERSECTED !== intersects[0].object) {
+          if (INTERSECTED) {
+            INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+          }
+
+          INTERSECTED = intersects[0].object;
+          INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+          INTERSECTED.material.emissive.setHex(0x08fbff);
+        }
+      } else {
+        if (INTERSECTED) {
+          INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+        }
+        INTERSECTED = null;
+      }
+      renderer.render(scene, camera);
+    }
+
+    function init() {
+      scene.background = new Color(0x000000);
+      scene.fog = new Fog(0x050505, 2000, 3500);
+      scene.add(new AmbientLight(0x444444));
+      directionalLight.position.set(0, -1, 0);
+      scene.add(directionalLight);
+
+      controls.enableZoom = true;
+      controls.enablePan = false;
+      controls.enableRotate = true;
+
+      var xLen = 20;
+      var offset = 5;
+      const texture = new TextureLoader().load('assets/images/thumbnails/placeholder.jpeg');
+      var geometry = new BoxGeometry(xLen, xLen, 1);
+      for (let i = 0; i < 3; i++) {
+        const mesh = new Mesh(geometry, new MeshLambertMaterial({ map: texture }));
+        mesh.userData.url = '/gallery/artwork' + (i + 1) + '/';
+        mesh.userData.isSelected = false;
+        mesh.position.x = (-xLen - offset) * (i - 1);
+        group.add(mesh);
+      }
+      scene.add(group);
+
+      geometry = new PlaneGeometry(500, 500, 10, 10);
+      let geo = new WireframeGeometry(geometry);
+
+      matLineBasic = new LineBasicMaterial({ color: 0x9c08ff });
+
+      wireframe = new LineSegments(geo, matLineBasic);
+      wireframe.computeLineDistances();
+      wireframe.visible = true;
+      wireframe.rotateX(Math.PI / 2);
+      wireframe.position.y -= xLen + offset;
+      scene.add(wireframe);
+
+      const renderScene = new RenderPass(scene, camera);
+
+      const bloomPass = new UnrealBloomPass(new Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+      bloomPass.threshold = params.bloomThreshold;
+      bloomPass.strength = params.bloomStrength;
+      bloomPass.radius = params.bloomRadius;
+
+      composer = new EffectComposer(renderer);
+      composer.addPass(renderScene);
+      composer.addPass(bloomPass);
+
+      camera.position.set(0, 0, 50);
+      camera.lookAt(new Vector3(0, 0, 0));
+    }
+
+    init();
+    animate();
+  }, []);
+  return (
+    <>
+      <div ref={inputEl}></div>
+    </>
+  );
 }
+
+export default Art;
