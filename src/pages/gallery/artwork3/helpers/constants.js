68e878d5da4c9dc81a4e5e029ea822bb50e40198
miatang13 <miatang13@gmail.com>
2021-05-25 07:19:54 -0700

    add folders

---
+++ src/pages/gallery/artwork3/helpers/constants.js
@@ -0,0 +1,72 @@
+//----------------------------------
+//  THREE JS SETUP
+//----------------------------------
+
+const colors = {
+  white: 0xffffff,
+  black: 0x0,
+  turquoise: 0x42f5b6,
+  pink: 0xffd1dc,
+};
+const cameraPositions = {
+  topDown: [0, 600, 0],
+  Angled: [0, 600, 600],
+};
+const options = {
+  backgroundColor: `lightblue`,
+  antialias: true,
+  cameraPosition: cameraPositions.topDown,
+  grid: {
+    size: 1000,
+    divisions: 50,
+  },
+  fog: {
+    near: 0.5,
+    far: 1000,
+  },
+};
+
+//----------------------------------
+//  ANIMATE
+//----------------------------------
+const destPos = { x: 0, y: 0, z: 0 };
+
+//----------------------------------
+//  OIMO PHYSICS
+//----------------------------------
+const groundInfo = {
+  size: [800, 40, 800],
+  pos: [0, -80, 0],
+};
+/* information for circle seeds */
+const destPosSets = [
+  new THREE.Vector3(0, 0, 0),
+  new THREE.Vector3(300, 0, 300),
+  new THREE.Vector3(-300, 0, -300),
+  new THREE.Vector3(300, 0, -300),
+  new THREE.Vector3(-300, 0, 300),
+];
+const startY = 300;
+const seed = [
+  {
+    r: 60,
+    x: -300,
+    y: startY,
+    z: -300,
+  },
+  {
+    r: 60,
+    x: 300,
+    y: startY,
+    z: -300,
+  },
+  {
+    r: 70,
+    x: 0,
+    y: startY,
+    z: 300,
+  },
+];
+const seedOpts = [seed, seed, seed, seed, seed];
+
+export { colors, options, groundInfo, destPosSets, seedOpts };
---
+++ src/pages/gallery/artwork3/helpers/functions.js
@@ -0,0 +1,22 @@
+import Circle from "./circle.js";
+
+const dist = (x1, y1, x2, y2) => Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+const hasTween = (element) => TweenMax.isTweening(element.position);
+const isAsleep = (bod) => bod.sleeping;
+
+function meshToCircle(mesh) {
+  const c = new Circle(
+    mesh.scale.x,
+    new Complex(mesh.position.x, mesh.position.z),
+    0
+  );
+  return c;
+}
+
+function meshesToCircles(meshesL) {
+  let circlesL = [];
+  meshesL.forEach((mesh) => circlesL.push(meshToCircle(mesh)));
+  return circlesL;
+}
+
+export { dist, hasTween, meshesToCircles, isAsleep };
