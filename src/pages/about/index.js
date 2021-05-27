a88c1adc32b5b95677aab8f8b7e2543a5fbb52a0
miatang13 <miatang13@gmail.com>
2021-05-27 09:54:25 -0700

    Add about content, styling

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -14,9 +14,19 @@ function About() {
     <main className="About">
       <Head title="About" />
 
-      <div className={styles.hero}>
+      <div className={styles.About}>
         <h1 className={styles.title}>About</h1>
-        <p> Welcome to the intern Gallery</p>
+        <p>Welcome to the WE3 Creative Gallery.</p>{' '}
+        <p>
+          We are Mariana de Queiroz, Amna Azhar, and Mia Tang, the WE3 developer interns for summer 2021. This website
+          contains artwork made by each of us using THREE.JS, which we got to learn together as a team.{' '}
+        </p>
+        <p>
+          This is the onboarding project for WE3 developer internship. It is a twofold project that involves working
+          with the boilerplate and creating our own artwork, thus being a creator and a curator at the same time. We
+          were given 3 weeks to complete the project under the guidance of our wonderful mentors: Amélie Maia and Fabio
+          Toste.
+        </p>
       </div>
     </main>
   );
